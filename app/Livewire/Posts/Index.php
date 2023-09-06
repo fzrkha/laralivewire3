a165953bc0cefc26ffd4833a0e56cc639417220f
fzrkha <redfanz02@gmail.com>
2023-09-06 10:48:20 +0700

    sudah selesai tahap 1

---
+++ app/Livewire/Posts/Index.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire\Posts;
+
+use App\Models\Post;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithPagination;
+
+    public function destroy($id) {
+        Post::destroy($id);
+
+        session()->flash('message', 'Data Berhasil Dihapus.');
+
+        return redirect()->route('posts.index');
+    }
+
+    public function render()
+    {
+        return view('livewire.posts.index', [
+            'posts' => Post::latest()->paginate(5)
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', App\Livewire\Posts\Index::class)->name('posts.index');
+Route::get('/create', App\Livewire\Posts\Create::class)->name('posts.create');
+Route::get('/edit/{id}', App\Livewire\Posts\Edit::class)->name('posts.edit');
